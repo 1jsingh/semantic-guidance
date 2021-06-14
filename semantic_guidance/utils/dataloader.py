3c3a7e0e5784167a7c3270dcc89bd62e91121109
SINGH JASKIRAT <jsingh@SINGHs-MacBook-Pro.local>
2021-06-14 21:33:28 +1000

    fix: dataloader grid

--- semantic_guidance/utils/dataloader.py
+++ semantic_guidance/utils/dataloader.py
@@ -58,21 +58,6 @@ class ImageDataset:
         # read rgb image
         img_name = self.img_list[idx]
         image = cv2.cvtColor(cv2.imread(img_name), cv2.COLOR_BGR2RGB)
-
-        # create grid
-        x, y, w, h = self.bbox_list[idx]
-        H, W, C = image.shape
-        x, y, w, h = x / W, y / H, w / W, h / H
-        if flip_horizontal:
-            x = 1 - x - w
-        Affine_Mat_w = [w, 0, (2 * x + w - 1)]
-        Affine_Mat_h = [0, h, (2 * y + h - 1)]
-        M = np.c_[Affine_Mat_w, Affine_Mat_h].T
-        M = torch.tensor(M).unsqueeze(0)
-        grid = torch.nn.functional.affine_grid(M, (1, 3, 128, 128))  # (1,128,128,2)
-        grid = (grid + 1) / 2  # scale between 0,1
-        grid = torch.tensor(grid * 255, dtype=torch.uint8).permute(0, 3, 1, 2)
-
         image = cv2.resize(image, (self.width, self.width))
         # image = adjust_gamma(image, gamma=1.5)
         if flip_horizontal:
@@ -80,7 +65,7 @@ class ImageDataset:
         image = torch.tensor(image, dtype=torch.uint8).permute(2, 0, 1)
 
         # initialize gbp, seg_img image
-        gbp_img, seg_img = None, None
+        gbp_img, seg_img, grid = None, None, None
 
         # read gbp image
         if self.gbp_list is not None:
@@ -100,5 +85,19 @@ class ImageDataset:
                 seg_img = cv2.flip(seg_img, 1)
             # convert to tensor
             seg_img = torch.tensor(seg_img, dtype=torch.uint8).unsqueeze(0)
+
+            # create grid
+            x, y, w, h = self.bbox_list[idx]
+            H, W, C = image.shape
+            x, y, w, h = x / W, y / H, w / W, h / H
+            if flip_horizontal:
+                x = 1 - x - w
+            Affine_Mat_w = [w, 0, (2 * x + w - 1)]
+            Affine_Mat_h = [0, h, (2 * y + h - 1)]
+            M = np.c_[Affine_Mat_w, Affine_Mat_h].T
+            M = torch.tensor(M).unsqueeze(0)
+            grid = torch.nn.functional.affine_grid(M, (1, 3, 128, 128))  # (1,128,128,2)
+            grid = (grid + 1) / 2  # scale between 0,1
+            grid = torch.tensor(grid * 255, dtype=torch.uint8).permute(0, 3, 1, 2)
 
         return image, gbp_img, seg_img, grid
